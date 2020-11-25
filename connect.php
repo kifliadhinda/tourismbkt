b4c1665c0f8d779fdcc215529e9d1e8966c144f9
dhindakiflia <63203788+dhindakiflia@users.noreply.github.com>
2020-11-25 22:01:20 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
-	$host = "localhost";
-	$user = "postgres";
-	$pass = "password";
+	$host = "ec2-54-84-98-18.compute-1.amazonaws.com";
+	$user = "bszykbesevjifb";
+	$pass = "5190a566577d022e6c5fe196fa4f061d1875d4a93c510100fe00bb743c13111e";
 	$port = "5432";
-	$dbname = "tgsiisi";
+	$dbname = "d22m1rf3ktnlg1";
 	$conn = pg_connect("host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$pass) or die("Gagal");
 	// header("Access-Control-Allow-Origin: *");
 ?>
