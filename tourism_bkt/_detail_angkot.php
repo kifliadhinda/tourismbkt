0f10ed48ab8f35393ac15c74839cf08df441ec20
dhindakiflia <63203788+dhindakiflia@users.noreply.github.com>
2020-11-26 22:04:43 +0700

    Update _detail_angkot.php

--- tourism_bkt/_detail_angkot.php
+++ tourism_bkt/_detail_angkot.php
@@ -1,6 +1,6 @@
 <?php
     include("../connect.php");
-    $id_tourism = @$_GET['id_tourism'];
+    $id_tourism = @$_GET['id'];
 
     $result=  pg_query("SELECT detail_tourism.id_angkot, tourism.name, angkot.destination, angkot.route_color as color, st_x(st_centroid(tourism.geom)) as lng2, st_y(st_centroid(tourism.geom)) as lat2, detail_tourism.lat, detail_tourism.lng, detail_tourism.description FROM detail_tourism left join tourism on tourism.id = detail_tourism.id_tourism left join angkot on angkot.id = detail_tourism.id_angkot where detail_tourism.id_tourism='$id_tourism' ");
 
