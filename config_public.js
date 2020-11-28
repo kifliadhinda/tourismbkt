b3d1d0e7b770496b54738e32b7d91c342e6fd571
kifliadhinda <75132887+kifliadhinda@users.noreply.github.com>
2020-11-28 11:32:34 +0700

    Update config_public.js

--- config_public.js
+++ config_public.js
@@ -2,6 +2,6 @@
 var ipServerHotel="http://gissurya.org/hotel_bkt/";
 var ipServerTourism="http://gissurya.org/tourism_bkt/";*/
 
-var ipServerAngkot="http://dhindakiflia-tourism.herokuapp.com//angkot_bkt/";
-var ipServerHotel="http://dhindakiflia-tourism.herokuapp.com/hotel_bkt/";
-var ipServerTourism="http://dhindakiflia-tourism.herokuapp.com/tourism_bkt/";
+var ipServerAngkot="https://dhindakiflia-tourism.herokuapp.com//angkot_bkt/";
+var ipServerHotel="https://dhindakiflia-tourism.herokuapp.com/hotel_bkt/";
+var ipServerTourism="https://dhindakiflia-tourism.herokuapp.com/tourism_bkt/";
